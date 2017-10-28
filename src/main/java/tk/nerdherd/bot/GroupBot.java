1241aa7ec363ba0cdd6f47cdc25b5fded044b809
root <root@devian>
2017-10-27 23:10:58 -0700

    Started complete re-write of the bot.
    Got basic group creation working

--- src/main/java/tk/nerdherd/bot/GroupBot.java
+++ src/main/java/tk/nerdherd/bot/GroupBot.java
@@ -1,252 +1,236 @@
-/**
- * 
- */
 package tk.nerdherd.bot;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Category;
+import net.dv8tion.jda.core.entities.Emote;
 import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageHistory;
+import net.dv8tion.jda.core.entities.PrivateChannel;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberRoleAddEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.managers.GuildController;
 
-/**
- * @author root
- *
- */
-public class GroupBot extends Bot implements BotInterface {
+public class GroupBot extends ListenerAdapter{
+	private static final String GROUP_CHANNEL_NAME = "groups";
+	private static final String REACTION_ADD_VOICE_CHANNEL = "🎙";
+	private static final String REACTION_JOIN_GROUP = "👤";
+	private static final String REACTION_ADD_TEXT_CHANNEL = "💬";
+	private static final String NEW_GUILD_OWNER_MESSAGE = "This bot requires a channel called 'groups' to perform all of it's functions.";
+	private static final String NEW_GUILD_NO_CHANNEL = "Please create a new text channel anywhere in your server by that name and I can get started. Note that there needs to be only one channel by this name or bad things will happen!";
+	private static final String NEW_GUILD_YES_CHANNEL = "Is this channel made for this bot?";
+	private static final String REACTION_YES = "✅";
+	private static final String REACTION_NO = "❎";
+	private static final String DEFAULT_CHANNEL_NAME = "general";
+	private static JDA jda;
 
-	private final String HELP_TEXT = "commands:\ncreate - create a new group\njoin - join a group\nleave - leave a group\ndelete - delete a group";
-	private final String LEAVE = "leave";
-	private final String ALIAS = "group";
-	private final String GROUP_CHANNEL_NAME = "groups";
-	private final String CREATE = "create";
-	private final String JOIN = "join";
-	private final String DELETE = "delete";
-	private final int MESSAGE_ID = 0;
-	private final int ROLE_NAME = 1;
-	private final int ROLE_ID = 2;
-	private final int NOTHING = 0;
-	private TextChannel groupChannel;
-	private GuildController guildController;
-	private ArrayList<ArrayList<String>> groups = new ArrayList<>();
-
-	/**
-	 * @param aGuild
-	 */
-	public GroupBot(Guild aGuild) {
-		super(aGuild);
-		guildController = guild.getController();
-
+	public static void main(String[] args) {
+		
+		/*
+		 * Attempt to log in to bot account and register our event listener
+		 */
 		try {
-			groupChannel = guild.getTextChannelsByName(GROUP_CHANNEL_NAME, false).get(0);
-		} catch (Exception e) {
-			groupChannel = (TextChannel) guildController.createTextChannel(GROUP_CHANNEL_NAME)
-					.addPermissionOverride(guild.getPublicRole(), Permission.MESSAGE_READ.getRawValue(),
-							Permission.MESSAGE_WRITE.getRawValue())
-					.complete();
-		}
-
-		for (Message message : groupChannel.getHistory().retrievePast(100).complete()) {
-			Scanner in = new Scanner(message.getContent());
-			ArrayList<String> group = new ArrayList<String>();
-			group.add(message.getId());
-			while (in.hasNext()) {
-				group.add(in.next());
-			}
-			groups.add(group);
-			in.close();
-		}
-
-		System.out.println(groups);
+			jda = new JDABuilder(AccountType.BOT).setToken(args[0]).buildBlocking();
+			jda.addEventListener(new GroupBot());
+		} catch (LoginException e) {
+			// TODO
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO
+			e.printStackTrace();
+		} catch (RateLimitedException e) {
+			// TODO
+			e.printStackTrace();
+		}	
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see tk.nerdherd.bot.BotInterface#command(net.dv8tion.jda.core.entities.
-	 * Message)
-	 */
+	
 	@Override
-	public void command(Message message) {
-		Scanner in = new Scanner(message.getContent());
-		in.next();
-		String cmd = in.next();
-		String arg = in.nextLine().trim();
-		in.close();
-		if (arg.matches("[^a-zA-Z0-9]"))
-			return;
-
-		if (cmd.equals(CREATE)) {
-			create(arg, message.getMember());
-		} else if (cmd.equals(JOIN)) {
-			join(arg, message.getMember());
-		} else if (cmd.equals(LEAVE)) {
-			leave(arg, message.getMember());
-		} else if (cmd.equals(DELETE)) {
-			delete(arg, message.getMember());
-		} else {
-			help();
+	public void onGuildMemberRoleAdd(GuildMemberRoleAddEvent event) {
+		if (event.getRoles().get(0).getName().equals(jda.getSelfUser().getName())) {
+			onJoinNewServer(event);
 		}
+	}
+	
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		if(event.getChannel().getName().equals(GROUP_CHANNEL_NAME)) {
+			newGroup(event.getMessage());
+		}
+	}
+	
+	@Override
+	public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
+		//Don't react to reactions from the bot itself
+		if(event.getUser().equals(jda.getSelfUser()))return;
+		
+		//Determine what to do based on the reaction
+		String reaction=event.getReactionEmote().getName();
+		if(reaction.equals(REACTION_ADD_VOICE_CHANNEL)) addVoiceChannel(event);
+		else if(reaction.equals(REACTION_JOIN_GROUP)) joinGroup(event);
+		else if(reaction.equals(REACTION_ADD_TEXT_CHANNEL)) addTextChannel(event);
+		else if(event.getMember().isOwner() && event.getChannel().getName().equals(GROUP_CHANNEL_NAME)) {
+			if (reaction.equals(REACTION_YES)) onJoinNewServer(event,true);
+			else if (reaction.equals(REACTION_NO)) onJoinNewServer(event,false);
+		}
+		
+		System.out.println(reaction);
 	}
 
 	/**
+	 * @param event 
+	 * @param b
+	 */
+	private void onJoinNewServer(GuildMessageReactionAddEvent event, boolean b) {
+		if(b) {
+			for(Message message : new MessageHistory(event.getChannel()).retrievePast(100).complete()) {
+				newGroup(message);
+			}
+		}else {
+			event.getChannel().sendMessage(NEW_GUILD_NO_CHANNEL);
+		}
+		
+	}
+
+	/**
+	 * @param event
+	 */
+	private void addTextChannel(GuildMessageReactionAddEvent event) {
+		// TODO
+		
+	}
+
+	/**
+	 * @param event 
 	 * 
 	 */
-	private void help() {
-		botChannel.sendMessage(HELP_TEXT);
-
+	private void addVoiceChannel(GuildMessageReactionAddEvent event) {
+		// TODO
+		System.out.println("add voice channel to group");
 	}
 
 	/**
-	 * @param arg
-	 * @param member
+	 * @param event
 	 */
-	private void delete(String arg, Member member) {
-		if (member.hasPermission(Permission.MANAGE_CHANNEL)) {
-			for (ArrayList<String> group : groups) {
-				if (group.get(ROLE_NAME).equals(arg)) {
-					guild.getRoleById(group.get(ROLE_ID)).delete().queue();
-					for (String channelId : group.subList(3, group.size())) {
-						try {
-							guild.getTextChannelById(channelId).delete().queue();
-						} catch (Exception e) {
-							guild.getVoiceChannelById(channelId).delete().queue();
-						}
-					}
-					groups.remove(group);
-					groupChannel.getMessageById(group.get(MESSAGE_ID)).complete().delete().queue();
-					return;
-				}
-			}
+	private void joinGroup(GuildMessageReactionAddEvent event) {
+		// TODO
+		
+	}
+
+	/**
+	 * @param message
+	 */
+	private void newGroup(Message message) {
+		// TODO create a group
+		if(message.getContent().equals(NEW_GUILD_NO_CHANNEL)) {message.delete().queue(); return;}
+		if(message.getContent().equals(NEW_GUILD_YES_CHANNEL)) {message.delete().queue(); return;}
+		if(message.getContent().equals(NEW_GUILD_OWNER_MESSAGE)) {message.delete().queue(); return;}
+		
+		String groupName=getFirstWord(message.getContent());
+		Guild guild=message.getGuild();
+		GuildController guildController=guild.getController();
+		Role role=newRole(guild,guildController,groupName);
+		role.getManager().setName(groupName).complete();
+		Category category = newCategory(guild,guildController,groupName);
+		category.createPermissionOverride(guild.getPublicRole()).setDeny(Permission.MESSAGE_READ).queue();
+		category.createPermissionOverride(role).setAllow(Permission.MESSAGE_READ).queue();
+		category.createPermissionOverride(message.getMember()).setAllow(Permission.ALL_PERMISSIONS).queue();
+		category.createTextChannel(DEFAULT_CHANNEL_NAME).queue();
+		
+		message.addReaction(REACTION_JOIN_GROUP).queue();;
+		message.addReaction(REACTION_ADD_VOICE_CHANNEL).queue();
+		message.addReaction(REACTION_ADD_TEXT_CHANNEL).queue();
+	}
+
+	/**
+	 * @param content
+	 * @return
+	 */
+	private String getFirstWord(String content) {
+		String workValue=content.trim()+" ";
+		return workValue.substring(0, workValue.indexOf(' '));
+	}
+
+	/**
+	 * @param guild
+	 * @param guildController
+	 * @param groupName
+	 * @return
+	 */
+	private Role newRole(Guild guild, GuildController guildController, String groupName) {
+		for(Role role : guild.getRoles()) {
+			if(role.getName().equals(groupName)) return role;
 		}
+		
+		return (Role) guildController.createRole().complete();
 	}
 
 	/**
-	 * @param arg
-	 * @param member
+	 * @param guild 
+	 * @param guildController 
+	 * @param groupName
+	 * @return
 	 */
-	private void leave(String arg, Member member) {
-		for (ArrayList<String> group : groups) {
-			if (group.get(ROLE_NAME).equals(arg)) {
-				guildController.removeRolesFromMember(member, guild.getRoleById(group.get(ROLE_ID))).queue();
-			}
+	private Category newCategory(Guild guild, GuildController guildController, String groupName) {
+
+		for(Category category : guild.getCategories()) {
+			if(category.getName().equals(groupName)) return category;
 		}
-
+		
+		return (Category) guildController.createCategory(groupName).complete();
+		
 	}
 
 	/**
-	 * @param arg
-	 * @param member
+	 * @param event
 	 */
-	@SuppressWarnings("serial")
-	private void create(String arg, Member member) {
-
-		Role role = role(arg);
-
-		for (List<String> group : groups) {
-			if (group.get(ROLE_NAME).equals(arg)) {
-				join(role.getName(), member);
+	private void onJoinNewServer(GuildMemberRoleAddEvent event) {
+		for(TextChannel textChannel : event.getGuild().getTextChannels()) {
+			if(textChannel.getName().equals(GROUP_CHANNEL_NAME)) {
+				askOwner(event,textChannel);
 				return;
+			}else {
+				askOwner(event);
 			}
-		}
-
-		List<TextChannel> channels = new ArrayList<TextChannel>();
-		textChannel(arg);
-
-		for (TextChannel tc : guild.getTextChannels()) {
-			if (tc.getName().startsWith(arg)) {
-				channels.add(tc);
-			}
-		}
-
-		String groupS = role.getName() + " " + role.getId();
-
-		try {
-			for (TextChannel tc : channels) {
-				tc.getPermissionOverride(guild.getPublicRole()).delete().complete();
-				tc.getPermissionOverride(role).delete().complete();
-			}
-		} catch (Exception e) {
-			//e.printStackTrace();
 		}
 		
-		try {
-		for (TextChannel channel : channels) {
-			channel.createPermissionOverride(guild.getPublicRole()).setDeny(Permission.MESSAGE_READ).queue();
-			channel.createPermissionOverride(role).setAllow(Permission.MESSAGE_READ).queue();
-			groupS += " " + channel.getId();
-		}
-
-		final String groupSF = groupS;
-
-		groups.add(new ArrayList<String>() {
-			{
-				add(groupChannel.sendMessage(groupSF).complete().getId());
-				add(role.getName());
-				add(role.getId());
-				for (TextChannel channel : channels) {
-					add(channel.getId());
-				}
-			}
-		});
-
-		join(role.getName(), member);
-		
-		}catch(Exception e) {
-			botChannel.sendMessage("Failed! please try again").queue();
-		}
-	}
-
-	private TextChannel textChannel(String arg) {
-
-		for (TextChannel tc : guild.getTextChannels()) {
-			if (tc.getName().equals(arg)) {
-				return tc;
-			}
-		}
-
-		return (TextChannel) guildController.createTextChannel(arg).complete();
-	}
-
-	private Role role(String arg) {
-
-		for (Role r : guild.getRoles()) {
-			if (r.getName().equals(arg)) {
-				return r;
-			}
-		}
-
-		return guildController.createRole().setName(arg).complete();
+		event.getGuild().getController().createTextChannel(GROUP_CHANNEL_NAME).queue();
+		event.getGuild().getOwner().getUser().openPrivateChannel().complete().sendMessage(NEW_GUILD_OWNER_MESSAGE);
 	}
 
 	/**
-	 * @param arg
-	 * @param member
-	 * @param author
+	 * @param event 
+	 * 
 	 */
-	private void join(String arg, Member member) {
-		for (ArrayList<String> group : groups) {
-			if (group.get(ROLE_NAME).equals(arg)) {
-				guildController.addRolesToMember(member, guild.getRoleById(group.get(ROLE_ID))).queue();
-			}
-		}
+	private void askOwner(GuildMemberRoleAddEvent event) {
+		PrivateChannel ownerChannel=event.getGuild().getOwner().getUser().openPrivateChannel().complete();
+		ownerChannel.sendMessage(NEW_GUILD_OWNER_MESSAGE);
+		ownerChannel.sendMessage(NEW_GUILD_NO_CHANNEL);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see tk.nerdherd.bot.BotInterface#getAlias()
+	/**
+	 * @param event 
+	 * @param textChannel
 	 */
-	@Override
-	public String getAlias() {
-		return ALIAS;
+	private void askOwner(GuildMemberRoleAddEvent event, TextChannel textChannel) {
+		textChannel.sendMessage(textChannel.getGuild().getOwner().getAsMention()+" "+NEW_GUILD_OWNER_MESSAGE).queue();
+		Message question=textChannel.sendMessage(NEW_GUILD_YES_CHANNEL).complete();
+		question.addReaction(REACTION_YES).queue();
+		question.addReaction(REACTION_NO).queue();
 	}
 
 }
